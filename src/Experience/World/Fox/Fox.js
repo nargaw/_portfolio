a5474028ca352a4829b1cd991cf6601cd15e3a53
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-05-31 14:58:40 -0400

    animations and controls

--- src/Experience/World/Fox/Fox.js
+++ src/Experience/World/Fox/Fox.js
@@ -120,33 +120,40 @@ export default class Fox
 
     update()
     {
+        this.foxControls.forward(this.foxBody)
+        this.foxControls.backward(this.foxBody)
+        this.foxControls.left(this.foxBody)
+        this.foxControls.right(this.foxBody)
+        this.foxControls.idle(this.foxBody)
         if(this.model){
             this.updatePosition()
         }
+        // //console.log(this.animation.actions.current)
+        // this.animation.mixer.update(this.time.delta * 0.001)
+        // this.foxControls.movement(this.foxBody, this.animation)
+        // //console.log(this.foxControls.keyMap)
+        // // if(this.foxControls.keyMap === true){
+        // //     console.log('true')
+        // // }
+        // if(this.foxControls.keyMap.w === true){
+        //     //this.animation.actions.current.stop()
+        //     //this.animation.play('walking')
+        //     this.animation.actions.current = this.animation.actions.walking
+        //     this.animation.actions.current.play()
+        // } 
 
-        this.animation.mixer.update(this.time.delta * 0.001)
-        this.foxControls.movement(this.foxBody, this.animation)
-        //console.log(this.foxControls.keyMap)
-        // if(this.foxControls.keyMap === true){
-        //     console.log('true')
+        // if(this.foxControls.keyMap.w === false){
+        //     console.log('here')
+        //     //this.animation.mixer.clipAction(this.resource.animations[0]).play()
+        //     // this.animation.actions.current.stop()
+        //     this.animation.actions.current = this.animation.actions.idle
+        //     this.animation.actions.current.play()
         // }
-        if(this.foxControls.keyMap.w === true){
-            //this.animation.actions.current.stop()
-            this.animation.play('walking')
-            // this.animation.actions.current = this.animation.actions.walking
-            // this.animation.actions.current.play()
-        } 
-
-        if(this.foxControls.keyMap.w === false){
-            console.log('here')
-            this.animation.actions.current = this.animation.actions.idle
-            this.animation.actions.current.play()
-        }
         
-        if (this.foxControls.keyMap === [' ']){
-            //this.animation.actions.current.stop()
-            console.log('stopped')
+        // if (this.foxControls.movement(this.foxBody, this.animation)){
+        //     //this.animation.actions.current.stop()
+        //     console.log('stopped')
             
-        }
+        // }
     }
 }
--- src/Experience/World/Fox/foxControls.js
+++ src/Experience/World/Fox/foxControls.js
@@ -1,55 +1,57 @@
 import Controls from "../../Utils/Controls";
 import Experience from "../../Experience";
+import * as THREE from 'three'
 
 export default class FoxControls extends Controls
 {
     constructor()
     {
         super()
-        this.experience = new Experience()
-        this.fox = this.experience.world.fox
+        this.experience = new Experience()    
         this.moving = false
     }
 
-    movement(body)
+    forward(body)
     {
-        let x = 0;
         if(this.keyMap['w'] || this.hoverMap['3']  || this.hoverTouch['3']|| this.keyMap['ArrowUp']){
             console.log('forward')
             body.position.z -= 0.005
             // animation.play('walking')
             this.moving = true
         }
+    }
 
+    backward(body)
+    {
         if(this.keyMap['s'] || this.hoverMap['4'] || this.hoverTouch['4'] || this.keyMap['ArrowDown']){
             console.log('backward')
-            body.position.z += 0.05
-        }
-
-        if(this.keyMap['a'] || this.hoverMap['1'] || this.hoverTouch['1']|| this.keyMap['ArrowLeft']){
-            console.log('left')
-            // // this.groundBody.quaternion.setFromEuler(Math.PI * 0.5, 0, 0)
-            // if(x <= Math.PI){
-            //     x = 1
-            //     console.log(x)
-            //     body.quaternion.y += x
-            // }
-            
-        }
-
-        if(this.keyMap['d'] || this.hoverMap['2'] || this.hoverTouch['2']|| this.keyMap['ArrowRight']){
-            console.log('right')
-            //body.quaternion.z += 0.1 * Math.PI
-        }
-
-        // if(this.moving === false)
-        // {
-        //     animation.play('idle')
-        //     console.log('here')
-        // }
-
-        if(this.keyMap[' ']){
-
+            body.position.z += 0.005
+            this.moving = true
         }
     }
+
+    left(body)
+    {
+        if(this.keyMap['a'] || this.hoverMap['1'] || this.hoverTouch['1']|| this.keyMap['ArrowLeft']){
+            console.log('left')
+            this.moving = true
+        } 
+    }
+
+    right(body)
+    {
+        if(this.keyMap['d'] || this.hoverMap['2'] || this.hoverTouch['2']|| this.keyMap['ArrowRight']){
+            console.log('right')
+            this.moving = true
+        }
+    }
+
+    idle(body)
+    {
+        if(this.keyMap['']){
+            this.moving = false
+            console.log('idle')
+        }
+    }
+
 }

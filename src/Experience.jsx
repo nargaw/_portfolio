27531a304516d4129ca35ba2400d8696d629745c
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-01-01 06:08:47 -0500

    added helicopter model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,13 +4,15 @@ import Lights from "./Lights"
 import TestObj from "./TestObj"
 // import { Physics, Debug } from "@react-three/cannon"
 import { Physics } from "@react-three/rapier"
+import Heli from "./Heli/Heli"
 
 export default function Experience()
 {
     return <>
         <Physics debug>
             <TestObj />
-            <Environment />        
+            <Environment /> 
+            <Heli />       
         </Physics>
         <Lights />
         

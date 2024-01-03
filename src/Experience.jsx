076ae169c73364e40e2e34737f5b216ae27535fd
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-01-03 05:03:21 -0500

    updated tail rotor and added instanced meshes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,12 +5,14 @@ import TestObj from "./TestObj"
 // import { Physics, Debug } from "@react-three/cannon"
 import { Physics } from "@react-three/rapier"
 import Heli from "./Heli/Heli"
+import InstancedObjs from "./InstancedObjs"
 
 export default function Experience()
 {
     return <>
         <Physics debug>
-            <TestObj />
+            {/* <TestObj /> */}
+            <InstancedObjs />
             <Environment /> 
             <Heli />       
         </Physics>
---
+++ src/InstancedObjs.jsx
@@ -0,0 +1,39 @@
+import { useRef, useMemo } from 'react'
+import {RoundCuboidCollider, BallCollider, CuboidCollider, InstancedRigidBodies, RigidBody } from '@react-three/rapier'
+import * as THREE from 'three'
+
+export default function InstancedObjs()
+{
+    const xGeometry = new THREE.BoxGeometry(2, 0.5, 0.5)
+    const material = new THREE.MeshStandardMaterial({color: 0x00ffff})
+
+    return <>
+
+        {[...Array(50)].map((value, index) =>
+            <RigidBody
+                key={index}
+                restitution={0.825}
+                friction={0.1}
+                colliders="cuboid"
+                position={[
+                    (Math.random() - 0.5) * 1,
+                    15 * index,
+                    (Math.random() - 0.5) +1
+                ]}
+            >
+                <group>
+                    <mesh 
+                        geometry={xGeometry}
+                        material={material}
+                        rotation-z={-Math.PI * 0.25}
+                    />
+                    <mesh
+                        geometry={xGeometry}
+                        material={material}
+                        rotation-z={Math.PI * 0.25}
+                    />
+                </group>
+            </RigidBody>  
+        )}
+    </>
+}

97e4730b1189a8faf90cb572a77d9358c75d141d
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-01-30 10:12:05 -0500

    update scene

--- src/PartOne/About.jsx
+++ src/PartOne/About.jsx
@@ -1,5 +1,5 @@
 import { InstancedRigidBodies, RigidBody, useRapier } from "@react-three/rapier"
-import { useMemo, useRef } from "react"
+import { useMemo, useRef, useEffect } from "react"
 import { useFrame, useThree } from "@react-three/fiber"
 import { Vector2, Vector3, Raycaster, MeshBasicMaterial } from "three"
 
@@ -8,7 +8,7 @@ export default function About()
 
     const raycaster = new Raycaster()
     const { rapier, world } = useRapier()
-    console.log(world)
+    // console.log(world)
 
     const cubes = useRef()
     const rigidBodies = useRef()
@@ -44,31 +44,48 @@ export default function About()
     window.addEventListener('pointermove', (e) => {
         pointer.x = (e.clientX / window.innerWidth) * 2 - 1
         pointer.y = -(e.clientY / window.innerHeight) * 2 + 1
-        // console.log(pointer)
     })
-
-    let cameraPosition = new Vector3()
-    let cameraDirection = new Vector3() 
-    const ray = new rapier.Ray()
-    useFrame((state) => {
-        
-         
-    })
-
-    // const origin = cameraPosition
-    // const direction = cameraDirection
-   
-    // const hit = world.castRay(ray)
-
     
+    // let current 
+
+    // useEffect(() => {
+    //     if(!rigidBodies.current){
+    //         return
+    //     }
+    //         rigidBodies.current[current].applyImpulse({
+    //             x: Math.random(),
+    //             y: Math.random(),
+    //             z: Math.random()
+    //         })
+    // }, []);
 
 
+
+    // useFrame((state) => {
+    //     // console.log(state.camera.position)
+    //     raycaster.setFromCamera(pointer, state.camera)
+    //     const intersection = raycaster.intersectObject(cubes.current)
+    //     // console.log(intersection)
+    //     if (intersection.length > 1){
+    //         const instanceId = intersection[0].instanceId
+    //         console.log(instanceId)
+    //         current = instanceId
+    //     }
+    // })
+    // window.addEventListener('click', (e) =>{
+        
+    //     const ray = raycaster.setFromCamera(pointer, camera)
+    //     console.log(ray)
+    //     console.log(camera)
+    // })
+  
     return <>
         <InstancedRigidBodies 
             instances={instances} 
             type="dynamic"
             gravityScale={0}
             ref={rigidBodies}
+            
         >
             <instancedMesh 
                 ref={cubes}

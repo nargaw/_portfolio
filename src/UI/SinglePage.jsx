ad3f289c9efe2a31c8e64541e2d928fddb65583f
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-02-07 13:12:21 -0500

    update UI

--- src/UI/SinglePage.jsx
+++ src/UI/SinglePage.jsx
@@ -1,13 +1,34 @@
 export default function SinglePage()
 {
 
-    const header = {
+    const containerStyle = {
+        alignContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        gap: '32px',
+        height: 'min-content',
+        justifyContent: 'center',
+        overflow: 'visible',
+        padding: '16px',
+        position: 'relative',
+        // width: '100%'
+    }
+
+    const headerStyle = {
         alignItems: 'flex-start',
         display: 'flex',
         flex: 'none',
         flexDirection: 'column',
         flexWrap: 'nowrap',
-        gap: '16px'
+        gap: '16px',
+        height: 'min-content',
+        width: '100%',
+        justifyContent: 'center',
+        overflow: 'visible',
+        padding: 0,
+        positin: 'relative'
     }
 
     const titleStyle = {
@@ -33,11 +54,12 @@ export default function SinglePage()
     }
 
     return <>
-        <div style={header}>
-            <h1 style={titleStyle}>Nate Argaw</h1>
-            <div style={titleDescription}>3D Front-end developer</div>
-            <div style={titleDetail}>Passionate about solving real-world problems using software. Adept at utilizing cutting-edge web technologies to create immersive, engaging and interactive experiences. Dedicated to building and supporting the community.</div>
+        <div style={containerStyle} className="container">
+            <div style={headerStyle}>
+                <h1 style={titleStyle}>Nate Argaw</h1>
+                <div style={titleDescription}>3D Front-end developer</div>
+                <div style={titleDetail}>Passionate about solving real-world problems using software. Adept at utilizing cutting-edge web technologies to create immersive, engaging and interactive experiences. Dedicated to building and supporting the community.</div>
+            </div>
         </div>
-        
     </>
 }
